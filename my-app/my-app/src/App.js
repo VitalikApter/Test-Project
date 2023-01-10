d7ad6a47410b357c5788016a967b308c32083b2f
VitalikApter <aptvetal2014@gmail.com>
2023-01-10 15:22:57 +0200

    React

---
+++ my-app/my-app/src/App.js
@@ -0,0 +1,25 @@
+import Logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-center">
+      <header className="app-header">
+        <Logo className="app-logo" title="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="app-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
